9b0559401cfcdf34f46b2731891d7c4e80ca9f7d
Elena Zaytseva <139309253+HelenZA4@users.noreply.github.com>
2023-08-24 02:36:02 +0700

    Update MainPage.java

--- src/main/java/mantis/pages/MainPage.java
+++ src/main/java/mantis/pages/MainPage.java
@@ -1,5 +1,6 @@
 package mantis.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -16,6 +17,27 @@ public class MainPage {
     @FindBy(css = "a[href='/mantisbt/view_all_bug_page.php']")
     private WebElement viewIssuesPageButton;
 
+    @FindBy(css = "a[href='/mantisbt/bug_report_page.php']")
+    private WebElement reportIssuesPageButton;
+
+    @FindBy(xpath = "//*[contains (text(), 'Assigned to Me')]")
+    private WebElement assignedToMeBlock;
+
+    @FindBy(xpath = "//*[contains (text(), 'Unassigned')]")
+    private WebElement unassignedBlock;
+
+    @FindBy(xpath = "//*[contains (text(), 'Reported by Me')]")
+    private WebElement reportedByMe;
+
+    @FindBy(xpath = "//*[contains (text(), 'Recently Modified')]")
+    private WebElement recentlyModified;
+
+    @FindBy(css = "tr:nth-child(1) > td.column-id > a")
+    private WebElement newIssueId;
+
+    @FindBy(css = "input[value='Delete Issues']")
+    private WebElement deleteIssuesButton;
+
     public MainPage(WebDriver driver) {
         this.driver = driver;
         wait = new WebDriverWait(driver, 30, 500);
@@ -26,7 +48,35 @@ public class MainPage {
         return userName.getText();
     }
 
+    public boolean isAssignedToMeBlockDisplayed(){
+        return assignedToMeBlock.isDisplayed();
+    }
+
+    public boolean isUnassignedBlockDisplayed(){
+        return unassignedBlock.isDisplayed();
+    }
+
+    public String isReportedByMeDisplayed() {
+        return reportedByMe.getText();
+    }
+
+    public String getTitleRecentlyModifiedDisplayed(){
+        return recentlyModified.findElement(By.xpath("//h4//a")).getText();
+    }
+
     public void goToViewIssuesPage() {
         viewIssuesPageButton.click();
+    }
+
+    public void goToReportIssuesPage() {
+        reportIssuesPageButton.click();
+    }
+
+    public void goToSureToDeletePage() {
+        deleteIssuesButton.click();
+    }
+
+    public String getCreatedIssueId() {
+        return newIssueId.getText();
     }
 }
