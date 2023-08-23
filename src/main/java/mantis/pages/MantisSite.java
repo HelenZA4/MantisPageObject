51eb884783c3cf4e75f33a796e6eb8e94aeb3bf6
Elena Zaytseva <139309253+HelenZA4@users.noreply.github.com>
2023-08-24 02:36:43 +0700

    Update MantisSite.java

--- src/main/java/mantis/pages/MantisSite.java
+++ src/main/java/mantis/pages/MantisSite.java
@@ -7,13 +7,18 @@ public class MantisSite {
     private LoginPage loginPage;
     private PasswordPage passwordPage;
     private MainPage mainPage;
+    private ViewIssuesPage viewIssuesPage;
+    private ReportIssuesPage reportIssuesPage;
+    private SureToDeletePage sureToDeletePage;
 
     public MantisSite(WebDriver driver) {
         this.driver = driver;
-
         loginPage = new LoginPage(driver);
         passwordPage = new PasswordPage(driver);
         mainPage = new MainPage(driver);
+        viewIssuesPage = new ViewIssuesPage(driver);
+        reportIssuesPage = new ReportIssuesPage(driver);
+        sureToDeletePage = new SureToDeletePage(driver);
     }
 
     public void login(String login, String password) {
@@ -31,5 +36,17 @@ public class MantisSite {
 
     public MainPage getMainPage() {
         return mainPage;
+    }
+
+    public ViewIssuesPage getViewIssuesPage() {
+        return viewIssuesPage;
+    }
+
+    public ReportIssuesPage getReportIssuesPage() {
+        return reportIssuesPage;
+    }
+
+    public SureToDeletePage getSureToDeletePage() {
+        return sureToDeletePage;
     }
 }
