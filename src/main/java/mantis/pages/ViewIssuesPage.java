43ac2f6aa0a16fa696ceb0e2186694c69216ddea
Elena Zaytseva <139309253+HelenZA4@users.noreply.github.com>
2023-08-29 22:39:21 +0400

    Update ViewIssuesPage.java

--- src/main/java/mantis/pages/ViewIssuesPage.java
+++ src/main/java/mantis/pages/ViewIssuesPage.java
@@ -1,6 +1,7 @@
 package mantis.pages;
 
 import mantis.utils.UtilsTests;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -19,17 +20,20 @@ public class ViewIssuesPage {
     @FindBy(id = "summary")
     private WebElement summaryField;
 
-    @FindBy(css ="//div[@class='footer-content']")
+    @FindBy(xpath = "//div[@class='footer-content']")
     private WebElement tableFooter;
 
-    @FindBy(css = "input[value='Delete']")
+    @FindBy(xpath = "//input[@value='Delete']")
     private WebElement deleteButton;
 
-    @FindBy(css = "input[value='Delete Issues']")
+    @FindBy(xpath = "//input[@value='Delete Issues']")
     private WebElement deleteIssuesButton;
 
-    @FindBy(xpath="//tr[1]//td[@class='column-id']/a[1]")
+    @FindBy(xpath = "//tr[1]//td[@class='column-id']/a[1]")
     private WebElement issueId;
+
+    @FindBy(xpath = "//*[@id='bug_action']")
+    private WebElement viewingIssues;
 
     public ViewIssuesPage(WebDriver driver) {
         this.driver = driver;
@@ -37,17 +41,24 @@ public class ViewIssuesPage {
         PageFactory.initElements(driver, this);
     }
 
-
     public void scrollToTableFooter() throws InterruptedException {
-       UtilsTests.scrollToElement(driver, tableFooter);
+        UtilsTests.scrollToElement(driver, tableFooter);
     }
 
     public int countIssues() {
-   return issues.size();  }
-
-    public void deleteIssue(){
+        return issues.size();
     }
-    public boolean getDeletedIssueSummary() {
-        return summaryField.equals(true);
+    public void selectNewIssueId() {
+        issueId.click();
+    }
+    public void deleteLastIssue() {
+        deleteButton.click();
+        deleteIssuesButton.click();
+    }
+
+    public boolean isIssuePresent(String issueId) { return true;}
+
+    public String getTitleViewingIssues(){
+        return viewingIssues.findElement(By.xpath("//*[@id='bug_action']")).getText();
     }
 }
