124aa7a8b167f0edc7708f80b6f30990d876119d
Elena Zaytseva <139309253+HelenZA4@users.noreply.github.com>
2023-08-29 22:29:55 +0400

    Update ReportIssueTest.java

--- src/test/java/mantis/tests/ReportIssueTest.java
+++ src/test/java/mantis/tests/ReportIssueTest.java
@@ -18,17 +18,25 @@ public class ReportIssueTest extends BaseTest {
         mantisSite.getReportIssuesPage().createNewIssue("Summary Lesson 6 23-08-2023", "Description 23-08-2023");
         Thread.sleep(2000);
 
-        mantisSite.getReportIssuesPage().getCreatedIssueId();
+        String createdIssueId = mantisSite.getReportIssuesPage().getCreatedIssueId();
         Thread.sleep(2000);
 
-        mantisSite.getViewIssuesPage().deleteIssue();
-        Thread.sleep(2000);
+        mantisSite.getMainPage().goToViewIssuesPage();
 
-        mantisSite.getSureToDeletePage().getDeletedIssueSummary();
-        Thread.sleep(2000);
-
-        String title = mantisSite.getMainPage().getTitleRecentlyModifiedDisplayed();
         SoftAssertions softAssert = new SoftAssertions();
+        softAssert.assertThat(mantisSite.getViewIssuesPage().isIssuePresent(createdIssueId))
+                .as("Check issue presence").isTrue();
+
+        mantisSite.getViewIssuesPage().selectNewIssueId();
+
+        mantisSite.getViewIssuesPage().deleteLastIssue();
+
+        softAssert.assertThat(mantisSite.getViewIssuesPage().isIssuePresent(createdIssueId))
+                .as("Check issue presence after deletion").isTrue();
+
+        String title = mantisSite.getViewIssuesPage().getTitleViewingIssues();
         softAssert.assertThat(title).doesNotContain("Summary Lesson 6 23-08-2023");
+
+        softAssert.assertAll();
     }
 }
